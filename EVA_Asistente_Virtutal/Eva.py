598d6f6740c97345f75151a81b7f174ccecb477a
celinalopez <celina.lopez@alumnos.frm.utn.edu.ar>
2023-05-24 15:23:34 -0300

    cambios code

--- EVA_Asistente_Virtutal/Eva.py
+++ EVA_Asistente_Virtutal/Eva.py
@@ -1,54 +1,61 @@
-import datetime
-import speech_recognition as sr;
+import webbrowser
+import speech_recognition as sr
 import pyttsx3;
-import pywhatkit;
-import wikipedia;
-import webbrowser;
 
-name = 'eva';
-listener = sr.Recognizer();
+r = sr.Recognizer() 
 
 
 engine = pyttsx3.init();
 voices = engine.getProperty('voices');
-engine.setProperty('voice', voices[5].id);
+engine.setProperty('voice', voices[20].id);
+
 
 def talk(text):
     engine.say(text);
     engine.runAndWait();
 
-def listen():
-    try:
-        with sr.Microphone() as source:
-            print("Escuchando...");
-            voice = listener.listen(source);
-            rec = listener.recognize_google(voice, languaje="es-US");
-            rec = rec.lower();
-            if name in rec:
-                rec = rec.replace(name, '');
-                talk(rec);
-    except TimeoutError as msg:
-            print(msg);
-    
-    return rec;
-
 def run():
-    rec= listen();
-    if 'reproduce' in rec:
-        music = rec.replace('reproduce','');
-        print('Reproduciendo...' + music);
-        pywhatkit.playonyt(music);
-    elif 'hora' in rec:
-        hora = datetime.datetime().now().strftime('%H:%M');
-        talk('Son las '+ hora);
-    elif 'busca' in rec:
-        order = rec.replace('busca','');
-        info = wikipedia.summary(order, 1);
-        talk(info);
-    elif 'facebook' in rec:
-        fb = webbrowser.open('https://www.facebook.com/');
-    elif 'google' in rec:
-        gl = webbrowser.open('https://www.google.com.ar/')
+    while True:
+        with sr.Microphone() as source:
+            talk("Hola, soy EVA tu asistente por voz")
+            audio = r.listen(source)
     
+            try:
+                text = r.recognize_google(audio)
+                print('Has dicho: {}'.format(text))
+                print(text)
+                if "Amazon" in text:
+                    webbrowser.open('http://amazon.es')
+                if "noticias" in text:
+                    webbrowser.open('https://www.losandes.com.ar/')
+                if "Facebook" in text:
+                    webbrowser.open('www.facebook.com')
+                if "Google" in text:
+                    webbrowser.open('www.google.com')
+                if "que tal" in text:
+                    print("Bien y vos?")
+                if(text != ""):
+                    break
+                if("salir") in text:
+                    break
+            except:
+                print('No te he entendido')
 
-run();
+from tkinter import Tk, Button
+
+def onClosing():
+    root.destroy()
+    print("Ventana cerrada")
+    
+def press():
+    run()
+
+root = Tk()
+root.title("Uso de Boton")
+root.geometry("400x400")
+root.protocol("WM_DELETE_WINDOW",onClosing)
+
+button = Button(root,text="Iniciar", command=press, background="green", foreground="white")
+button.pack()
+
+root.mainloop()
